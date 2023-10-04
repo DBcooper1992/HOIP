e2d9f1d04917bb7540b534e96eae4d932078758e
nick.sui <nick.sui@shijigroup.com>
2023-10-04 11:40:09 +0800

    First commit

---
+++ src/main/java/priv/nicks/hoip/HoipApplication.java
@@ -0,0 +1,13 @@
+package priv.nicks.hoip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/priv/nicks/hoip/HoipApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.nicks.hoip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
